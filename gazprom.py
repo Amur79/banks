d4240ef7512f35d6eac7b8032fd3ca67eb946b6c
Amur79 <37978197+Amur79@users.noreply.github.com>
2020-10-03 16:04:15 +1000

    Add files via upload

--- gazprom.py
+++ gazprom.py
@@ -10,12 +10,6 @@ import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 
-# для карты
-import io
-import urllib.request
-import random
-import mercantile
- 
 from cairo import ImageSurface, FORMAT_ARGB32, Context
 
 
@@ -83,47 +77,6 @@ def image_bank(file_gazprom,file_opendata):
    plt.scatter(bankomats[:, 0], bankomats[:, 1], c='cyan', s=10)
    plt.scatter(dat.long, dat.lat, c='red', s=10)
 
-
-# отображение карты мспользуя OpenStreetMaps
-def plugmap(w,s,e,n):
-  west = open_df.long.min()
-  south =open_df.lat.min()
-  east = open_df.long.max()
-  north = open_df.lat.max()
-  zoom = 5 # 
-
-  tiles = list(mercantile.tiles(west, south, east, north, zoom))
-  print(tiles)
-
-  tile_size = (256, 256)
-  # создаем пустое изображение в которое как мозайку будем вставлять тайлы
-  # для начала просто попробуем отобразить все четыре тайла в строчку
-  map_image = ImageSurface(FORMAT_ARGB32, tile_size[0] * len(tiles), tile_size[1])
-
-  # создаем контекст для рисования
-  ctx = Context(map_image)
-
-  for idx, t in enumerate(tiles):
-    server = random.choice(['a', 'b', 'c'])  # у OSM три сервера, распределяем нагрузку
-    url = 'http://{server}.tile.openstreetmap.org/{zoom}/{x}/{y}.png'.format(
-        server=server,
-        zoom=t.z,
-        x=t.x,
-        y=t.y
-    )
-    # запрашиваем изображение
-    response = urllib.request.urlopen(url)
-
-    # создаем cairo изображние 
-    img = ImageSurface.create_from_png(io.BytesIO(response.read()))
-
-    # рисуем изображение, с правильным сдвигом по оси x
-    ctx.set_source_surface(img, idx * tile_size[0], 0)
-    ctx.paint()
-
-  # сохраняем собраное изображение в файл
-  with open("map.png", "wb") as f:
-    map_image.write_to_png(f)
 
 if __name__ == "__main__":
     image_bank(file_gazprom_csv,file_opendata_csv)
